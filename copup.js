052da661668018ac66a51970150b60c44de16a66
Godstime Eluro <140482555+Elurogodstime@users.noreply.github.com>
2025-02-27 11:50:40 -0800

    Add files via upload

---
+++ copup.js
@@ -0,0 +1,6 @@
+let first = document.getElementsByClassName("freq");
+let second = document.getElementsByClassName("h5")
+
+first.addEventListener("mouseover", showUs);
+function showUsQX"O
+function showUsQX"O
